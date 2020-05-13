ec449201dff34e03b32b204445ba9e0c7d3078f2
VasiliyVolkov <vasiliy.volkov88@gmail.com>
2020-05-12 20:55:39 -0700

    Step 3: updates

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ formElement.addEventListener("submit", e => {
 
   var small;
   var large;
-  let primeNums = [];
-  var divideCheck;
+  let evenNums = [];
 
   if(numOne > numTwo){
     small = numTwo;
@@ -19,22 +18,22 @@ formElement.addEventListener("submit", e => {
     large = numTwo
   }
  
-  if(numOne < 2 || numOne > 100){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 1 input ${numOne} is not in the range of 2 and 100</p>`;}
+  if(isNaN(numTwo)){document.getElementById("output").innerHTML+=`<p class="text-danger m-0">Number 2 input ${e.target.elements.numberTwo.value} is not a valid number</p>`;}
 
-  if(isNaN(numOne)){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 1 input ${e.target.elements.numberOne.value} is not a valid number</p>`;}
+  if(isNaN(numOne)){document.getElementById("output").innerHTML+=`<p class="text-danger m-0">Number 1 input ${e.target.elements.numberOne.value} is not a valid number</p>`;}
 
-  if(numTwo < 2 || numTwo > 100){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 2 input ${numTwo} is not in the range of 2 and 100</p>`;}
+  if(numOne < 2 || numOne > 100){document.getElementById("output").innerHTML+=`<p class="text-danger m-0">Number 1 input ${numOne} is not in the range of 2 and 100</p>`;}
 
-  if(isNaN(numTwo)){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 2 input ${e.target.elements.numberTwo.value} is not a valid number</p>`;}
+  if(numTwo < 2 || numTwo > 100){document.getElementById("output").innerHTML+=`<p class="text-danger m-0">Number 2 input ${numTwo} is not in the range of 2 and 100</p>`;}
   
   else if(numOne >= 2 && numOne <= 100 && numTwo >= 2 && numTwo <= 100){
     for(var i = small; i<=large;i+=1){
-      if(i%2===0){primeNums.push(i)}
+      if(i%2===0){evenNums.push(i)}
     }
-    document.getElementById("output").innerHTML=`<p class="text-info">There are ${primeNums.length} even numbers.</p>`;
+    document.getElementById("output").innerHTML=`<p class="text-info m-0">There are ${evenNums.length} even numbers.</p>`;
 
-    if(primeNums.length > 0){
-      document.getElementById("output").innerHTML+=`<p class="text-info">${primeNums.toString()}</p>`;
+    if(evenNums.length > 0){
+      document.getElementById("output").innerHTML+=`<p class="text-info m-0">${evenNums.toString()}</p>`;
     }
   
   
