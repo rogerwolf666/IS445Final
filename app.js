a3ea42d6f81135ca0b6199b71eda0aa0c156c61a
VasiliyVolkov <vasiliy.volkov88@gmail.com>
2020-05-12 20:41:45 -0700

    Step 2: fixed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ formElement.addEventListener("submit", e => {
   const numOne = Number(e.target.elements.numberOne.value)
   const numTwo = Number(e.target.elements.numberTwo.value)
   
+  {document.getElementById("output").innerHTML=""}
+
   var small;
   var large;
   let primeNums = [];
@@ -19,13 +21,13 @@ formElement.addEventListener("submit", e => {
  
   if(numOne < 2 || numOne > 100){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 1 input ${numOne} is not in the range of 2 and 100</p>`;}
 
-  else if(isNaN(numOne)){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 1 input ${e.target.elements.numberOne.value} is not a valid number</p>`;}
+  if(isNaN(numOne)){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 1 input ${e.target.elements.numberOne.value} is not a valid number</p>`;}
 
-  else if(numTwo < 2 || numTwo > 100){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 2 input ${numTwo} is not in the range of 2 and 100</p>`;}
+  if(numTwo < 2 || numTwo > 100){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 2 input ${numTwo} is not in the range of 2 and 100</p>`;}
 
-  else if(isNaN(numTwo)){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 2 input ${e.target.elements.numberTwo.value} is not a valid number</p>`;}
+  if(isNaN(numTwo)){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 2 input ${e.target.elements.numberTwo.value} is not a valid number</p>`;}
   
-  else {
+  else if(numOne > 2 && numOne < 100&&numTwo > 2 && numTwo < 100){
     for(var i = small; i<=large;i+=1){
       divideCheck=0
       for(var j = 2; j<i;j+=1){
