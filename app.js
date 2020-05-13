37d588961b9ba6424ab327750631c94588fb0978
VasiliyVolkov <vasiliy.volkov88@gmail.com>
2020-05-12 20:47:33 -0700

    Step 3: draft

--- app.js
+++ app.js
@@ -27,18 +27,14 @@ formElement.addEventListener("submit", e => {
 
   if(isNaN(numTwo)){document.getElementById("output").innerHTML+=`<p class="text-danger">Number 2 input ${e.target.elements.numberTwo.value} is not a valid number</p>`;}
   
-  else if(numOne > 2 && numOne < 100&&numTwo > 2 && numTwo < 100){
+  else if(numOne >= 2 && numOne <= 100 && numTwo >= 2 && numTwo <= 100){
     for(var i = small; i<=large;i+=1){
-      divideCheck=0
-      for(var j = 2; j<i;j+=1){
-        if(i%j===0){divideCheck+=1}
-      }
-      if(divideCheck===0){primeNums.push(i)}
+      if(i%2===0){primeNums.push(i)}
     }
-    document.getElementById("output").innerHTML=`<h5 class="text-info pl-3">There are ${primeNums.length} prime numbers.</h5>`;
+    document.getElementById("output").innerHTML=`<p class="text-info">There are ${primeNums.length} even numbers.</p>`;
 
     if(primeNums.length > 0){
-      document.getElementById("output").innerHTML+=`<p class="text-danger font-weight-bold pl-3">${primeNums.toString()}</p>`;
+      document.getElementById("output").innerHTML+=`<p class="text-info">${primeNums.toString()}</p>`;
     }
   
   
